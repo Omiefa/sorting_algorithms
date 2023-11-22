e72ddc4af762946034cd217f273a1df0a98d4559
Omiefa <bristolmelvin@yahoo.com>
2023-11-22 08:09:17 +0100

    task4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+  *swap - Function that swaps two integers
+  *@a: the first element to be swapped
+  *@b: the second element to be swapped
+  */
+
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * shell_sort - Sort an array of integers in ascending
  *              order using the shell sort algorithm.
  * @array: An array of integers.
